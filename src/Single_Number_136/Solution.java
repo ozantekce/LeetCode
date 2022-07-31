0e9777e6e20248bdffc4a2d52950005592ab3946
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-01 01:45:31 +0300

    136. Single Number
    slower but cooler

--- src/Single_Number_136/Solution.java
+++ src/Single_Number_136/Solution.java
@@ -1,37 +1,31 @@
 package Single_Number_136;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
 
     public static void main(String[] args) {
 
-        System.out.println(singleNumber(new int[]{2,2,1}));
+        System.out.println(singleNumber(new int[]{4,1,2,1,2}));
     }
 
     //11224
 
     public static int singleNumber(int[] nums) {
-
-        Arrays.sort(nums);
-        int n1,n2;
-        for (int i = 0; i < nums.length; i+=2) {
-            n1 = nums[i];
-            if(i+1==nums.length){
-                return n1;
+        
+        Set<Integer> set = new HashSet<>();
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(set.add(nums[i])){
+                result += nums[i];
+            }else{
+                result -= nums[i];
             }
-            n2 = nums[i+1];
-            if(n1!=n2)
-                return n1;
+
         }
-
-        return -1;
+        return result;
     }
-
 
 
 }
