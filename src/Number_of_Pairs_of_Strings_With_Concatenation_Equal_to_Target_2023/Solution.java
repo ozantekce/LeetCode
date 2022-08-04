315ee7bf3656ed0abb46139e68aeabca15452c3d
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-04 04:24:30 +0300

    2023. Number of Pairs of Strings With Concatenation Equal to Target

---
+++ src/Number_of_Pairs_of_Strings_With_Concatenation_Equal_to_Target_2023/Solution.java
@@ -0,0 +1,38 @@
+package Number_of_Pairs_of_Strings_With_Concatenation_Equal_to_Target_2023;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String[] nums = {"1","111"};
+        String target = "11";
+        System.out.println(numOfPairs(nums,target));
+
+    }
+
+    // O(n^2)
+    public static int numOfPairs(String[] nums, String target) {
+
+        int counter =0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < nums.length; j++) {
+                if(i==j)
+                    continue;
+                stringBuilder.delete(0,stringBuilder.length());
+                stringBuilder.append(nums[i]);
+                stringBuilder.append(nums[j]);
+                if(stringBuilder.toString().equals(target)){
+                    counter++;
+                }
+            }
+            
+        }
+        
+        return counter;
+    }
+
+}
