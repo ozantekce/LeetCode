24095f26df7f9ab78b5d72d132f230489f267efd
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-14 04:41:15 +0300

    778. Swim in Rising Water
    Better solution

--- src/Swim_in_Rising_Water_778/Solution.java
+++ src/Swim_in_Rising_Water_778/Solution.java
@@ -1,14 +1,12 @@
 package Swim_in_Rising_Water_778;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-
+/*
         int [][] grid = {
                 {0,1,2,3,4}
                 ,{24,23,22,21,5}
@@ -16,136 +14,141 @@ public class Solution {
                 ,{11,17,18,19,20}
                 ,{10,9,8,7,6}
         };
-
+*/
 /*
         int [][] grid = {
                 {3,2}
                 ,{0,1}
         };
-        System.out.println(swimInWater(grid));//3
 */
-/*
-        int [][] grid = {
-         {7,34,16,12,15,0}
-        ,{10,26,4,30,1,20}
-        ,{28,27,33,35,3,8}
-        ,{29,9,13,14,11,32}
-        ,{31,21,23,24,19,18}
-        ,{22,6,17,5,2,25}
+
+        int[][] grid = {
+                  {7, 34, 16, 12, 15, 0}
+                , {10, 26, 4, 30, 1, 20}
+                , {28, 27, 33, 35, 3, 8}
+                , {29, 9, 13, 14, 11, 32}
+                , {31, 21, 23, 24, 19, 18}
+                , {22, 6, 17, 5, 2, 25}
         };
+
+
+
         for (int i = 0; i < grid.length; i++) {
             System.out.println(Arrays.toString(grid[i]));
         }
         System.out.println("--------------------------------------------------------------------------------------------");
-        */
+
 
         System.out.println(swimInWater(grid));//
 
     }
 
 
-    private static int t=0;
-
-    private static List<Swimmer> swimmerList;
-    private static Swimmer [][] swimmers;
-
-    private static int gridArea[][];
     public static int swimInWater(int[][] grid) {
-        t=0;
-        swimmers = new Swimmer[grid.length][grid.length];
-        swimmerList = new ArrayList<>();
-        gridArea = grid;
-        Swimmer swimmer = new Swimmer(0,0);
-        swimmers[0][0] = swimmer;
-        swimmerList.add(swimmer);
 
-        while (swimmers[grid.length-1][grid.length-1]==null){
-            //int size = swimmerList.size();
-            t++;
-            for (int i = 0; i < swimmerList.size(); i++) {
-                swimmerList.get(i).diffusion();
+        int [] gridArea = new int[grid.length* grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                gridArea[j+ (i* grid.length)] = grid[i][j];
             }
-            //printer();
+        }
+
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        HashMap<Integer,Integer> valueIndexPair = new HashMap<>();
+
+        queue.add(gridArea[0]);
+        valueIndexPair.put(gridArea[0],0);
+
+        int max = Integer.MIN_VALUE;
+
+        while (true){
+
+            int currentVal = queue.poll();
+            int currentIndex = valueIndexPair.get(currentVal);
+
+            if(currentVal>max){
+                max = currentVal;
+            }
+
+            //System.out.println("cv : "+currentVal);
+            if(currentIndex==gridArea.length-1){
+                break;
+            }
+
+            if(hasDown(currentIndex,grid.length)){
+
+                if(!valueIndexPair.containsKey(gridArea[getDownIndex(currentIndex,grid.length)])){
+                   // System.out.println("down : "+gridArea[getDownIndex(currentIndex,grid.length)]);
+                    queue.add(gridArea[getDownIndex(currentIndex,grid.length)]);
+                    valueIndexPair.put(gridArea[getDownIndex(currentIndex,grid.length)],getDownIndex(currentIndex,grid.length));
+                }
+
+            }
+            if(hasUp(currentIndex,grid.length)){
+                if(!valueIndexPair.containsKey(gridArea[getUpIndex(currentIndex,grid.length)])){
+                    //System.out.println("up : "+gridArea[getUpIndex(currentIndex,grid.length)]);
+                    queue.add(gridArea[getUpIndex(currentIndex,grid.length)]);
+                    valueIndexPair.put(gridArea[getUpIndex(currentIndex,grid.length)],getUpIndex(currentIndex,grid.length));
+                }
+
+            }
+            if(hasRight(currentIndex,grid.length)){
+                if(!valueIndexPair.containsKey(gridArea[getRightIndex(currentIndex)])){
+                    //System.out.println("right : "+gridArea[getRightIndex(currentIndex)]);
+                    queue.add(gridArea[getRightIndex(currentIndex)]);
+                    valueIndexPair.put(gridArea[getRightIndex(currentIndex)],getRightIndex(currentIndex));
+                }
+            }
+            if(hasLeft(currentIndex,grid.length)){
+                if(!valueIndexPair.containsKey(gridArea[getLeftIndex(currentIndex)])){
+                    //System.out.println("left : "+gridArea[getLeftIndex(currentIndex)]);
+                    queue.add(gridArea[getLeftIndex(currentIndex)]);
+                    valueIndexPair.put(gridArea[getLeftIndex(currentIndex)],getLeftIndex(currentIndex));
+                }
+
+            }
+            //System.out.println(queue);
+
+
+
         }
 
 
 
-        return t;
+        return max;
     }
 
-    public static class Swimmer{
+    //6 6 6 6
+    //6,6,6 6
 
-        public int x,y;
+    private static boolean hasLeft(int index,int mul){
+        return (index%mul)-1>=0;
+    }
+    private static boolean hasRight(int index,int mul){
+        return (index%mul)+1<mul;
+    }
+    private static boolean hasUp(int index,int mul){
 
-        public Swimmer(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public void diffusion(){
-
-            if(gridArea[x][y]>t)
-                return;
-
-            if(t==26){
-                System.out.println("x : "+x+" y: "+y);
-            }
-
-            if(x+1<gridArea.length && gridArea[x+1][y]<=t){
-
-                if(swimmers[x+1][y]==null){
-                    swimmers[x+1][y] = new Swimmer(x+1,y);
-                    swimmerList.add(swimmers[x+1][y]);
-                    if(t==26){
-                        System.out.println("1");
-                    }
-                }
-            }
-            if(x-1>=0 && gridArea[x-1][y]<=t){
-                if(swimmers[x-1][y]==null){
-                    swimmers[x-1][y] = new Swimmer(x-1,y);
-                    swimmerList.add(swimmers[x-1][y]);
-                    if(t==26){
-                        System.out.println("2");
-                    }
-                }
-            }
-            if(y+1<gridArea.length && gridArea[x][y+1]<=t){
-                if(swimmers[x][y+1]==null){
-                    swimmers[x][y+1] = new Swimmer(x,y+1);
-                    swimmerList.add(swimmers[x][y+1]);
-                    if(t==26){
-                        System.out.println("3");
-                    }
-                }
-            }
-            if(y-1>=0 && gridArea[x][y-1]<=t){
-                if(swimmers[x][y-1]==null){
-                    swimmers[x][y-1] = new Swimmer(x,y-1);
-                    swimmerList.add(swimmers[x][y-1]);
-                    if(t==26){
-                        System.out.println("4");
-                    }
-                }
-            }
-
-        }
-
-        @Override
-        public String toString() {
-            return "pull";
-        }
+        return index>=mul;
     }
 
-    public  static void printer(){
-        System.out.println("t : "+t);
-        for (int i = 0; i < swimmers.length; i++) {
-            System.out.println(Arrays.toString(swimmers[i]));
-        }
-        System.out.println("--------------------------------------------------------------------------------------------");
+    private static boolean hasDown(int index,int mul){
+        return (index+mul)/mul<mul;
     }
 
 
-
+    private static int getLeftIndex(int index){
+        return index-1;
+    }
+    private static int getRightIndex(int index){
+        return index+1;
+    }
+    private static  int getUpIndex(int index,int mul){
+        return  index - mul;
+    }
+    private static int getDownIndex(int index,int mul){
+        return index + mul;
+    }
 
 }
