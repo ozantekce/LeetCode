f390f67719fb53baeb10c532ccf7afdca0edbc76
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-20 16:59:53 +0300

    2236. Root Equals Sum of Children

---
+++ src/Root_Equals_Sum_of_Children_2236/Solution.java
@@ -0,0 +1,34 @@
+package Root_Equals_Sum_of_Children_2236;
+
+
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public static boolean checkTree(TreeNode root) {
+
+
+        return root.val == (root.left.val+root.right.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
