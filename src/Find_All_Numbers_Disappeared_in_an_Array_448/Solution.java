71816213a8777d55080cac91d0730a1c911c4fb5
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-21 01:55:03 +0300

    Update Solution.java

--- src/Find_All_Numbers_Disappeared_in_an_Array_448/Solution.java
+++ src/Find_All_Numbers_Disappeared_in_an_Array_448/Solution.java
@@ -1,31 +1,52 @@
 package Find_All_Numbers_Disappeared_in_an_Array_448;
 
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
 
     public static void main(String[] args) {
 
-        int [] nums ={4,3,2,7,8,2,3,1};
+        int [] nums ={1,1};
         System.out.println(findDisappearedNumbers(nums));
 
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
 
-        ArrayList<Integer> list = new ArrayList<>(nums.length);
-        Set<Integer> set = new TreeSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
-        }
-        for (int i = 1; i < nums.length+1; i++) {
-            if(!set.contains(i)){
-                list.add(i);
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int pointer = 0;
+        int val =1;
+        while (true){
+            if(pointer>=nums.length){
+
+                if(val<=nums.length){
+                    for (int i = val; i <= nums.length; i++) {
+                        list.add(i);
+                    }
+                }
+
+                break;
             }
+            if(nums[pointer] == val){
+                val++;
+                pointer++;
+            }else if(nums[pointer]>val){
+                list.add(val);
+                val++;
+
+            }else{
+                pointer++;
+            }
+
         }
 
+        //System.out.println(list);
         return list;
     }
 
