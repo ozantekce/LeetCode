c63bf4c9715c6d5a2ff321974178baf26b6bbc02
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-24 10:52:19 +0300

    Update Solution.java

--- src/Check_If_N_and_Its_Double_Exist_1346/Solution.java
+++ src/Check_If_N_and_Its_Double_Exist_1346/Solution.java
@@ -1,29 +1,40 @@
 package Check_If_N_and_Its_Double_Exist_1346;
 
+import java.util.Arrays;
+
 public class Solution {
 
 
     public static void main(String[] args) {
 
         
-        System.out.println(checkIfExist(new int[]{3,1,7,11}));
+        System.out.println(checkIfExist(new int[]{-10,12,-20,-8,15}));
         
     }
 
     public static boolean checkIfExist(int[] arr) {
 
+        Arrays.sort(arr);
+
+        //4,6,10,20
+        //2,3,5,10
 
         for (int i = 0; i < arr.length; i++) {
 
-            for (int j = i+1; j < arr.length; j++) {
 
-                if(arr[i] == arr[j]*2 || arr[i]*2 == arr[j])
-                    return true;
+            int index = -1;
+            if(arr[i]>0){
+                index = Arrays.binarySearch(arr,i+1,arr.length,arr[i]*2);
+            }else{
+                index = Arrays.binarySearch(arr,0,i,arr[i]*2);
+            }
+
+            if(index>=0){
+                return true;
             }
 
         }
-        
-        
+
         return false;
     }
 
