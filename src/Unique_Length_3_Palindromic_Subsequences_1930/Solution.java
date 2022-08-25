df1b63c8324dc37d69da13af48f02c542907c179
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-08-25 14:48:11 +0300

    Update Solution.java

--- src/Unique_Length_3_Palindromic_Subsequences_1930/Solution.java
+++ src/Unique_Length_3_Palindromic_Subsequences_1930/Solution.java
@@ -8,46 +8,41 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(countPalindromicSubsequence("bbcbaba"));
+        System.out.println(countPalindromicSubsequence("adc"));
 
     }
 
     public static int countPalindromicSubsequence(String s) {
 
         char [] chars = s.toCharArray();
-        int totalNumOfChars =0;
-        //int aVal =97-97;//0
-        //int zVal =122-97;//25
+        int [] charFirstIndex = new int[26];
+        int [] charLastIndex = new int[26];
         int [] charCounter = new int[26];
         for (int i =0; i<s.length() ; i++) {
-            if(charCounter[s.charAt(i)-97]==0){
-                totalNumOfChars++;
+            int charVal = s.charAt(i)-97;
+            if(charCounter[charVal]==0){
+                charFirstIndex[charVal] = i;
             }
+            charLastIndex[charVal] = i;
             charCounter[s.charAt(i)-97]++;
-
         }
 
         char[] palindromic = new char[3];
         int val=0;
         for (int i = 0; i < 26; i++) {
-
-            char firstChar = (char) (i+97);
-            palindromic[0] = firstChar;
             if(charCounter[i]>=2){
-
+                char firstChar = (char) (i+97);
+                palindromic[0] = firstChar;
                 for (int j = 0; j < 26; j++) {
-                    char secondChar = (char)(j+97);
-                    palindromic[1] = secondChar;
                     if(charCounter[j]>0){
+                        char secondChar = (char)(j+97);
+                        palindromic[1] = secondChar;
                         palindromic[2] = firstChar;
-                        if(stringContains(palindromic,chars))
+                        if(stringContains(palindromic,chars,charFirstIndex[i],charLastIndex[i]))
                             val++;
                     }
-
                 }
-
             }
-
 
         }
 
@@ -56,7 +51,8 @@ public class Solution {
     }
 
 
-    private static boolean stringContains(char[] palindromic, char[] string){
+    private static boolean stringContains(char[] palindromic
+            , char[] string,int firstIndex,int lastIndex){
 
         char first = palindromic[0];
         boolean firstFound = false;
@@ -64,7 +60,7 @@ public class Solution {
         boolean secondFound= false;
         char third = palindromic[2];
 
-        for (int i = 0; i < string.length; i++) {
+        for (int i = firstIndex; i <= lastIndex; i++) {
 
             if(!firstFound){
                 if(string[i] == first)
