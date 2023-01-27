03b5ee11e4adcc98d907548569f6b3fc245b1ddd
ozantekce <ozantekce@gmail.com>
2023-01-27 05:28:16 +0300

    2288. Apply Discount to Prices

--- src/Apply_Discount_to_Prices_2288/Solution.java
+++ src/Apply_Discount_to_Prices_2288/Solution.java
@@ -4,7 +4,8 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println(discountPrices("$7383692 5q $5870426",100));
+
+        System.out.println(discountPrices("favsijl39l3ur$czt6rz hz $68050 lz9 ny 59o $4 $ $",9));
     }
 
     public enum State{
@@ -20,8 +21,14 @@ public class Solution {
 
         State state = State.none;
 
+        int recordStart = 0;
+        int recordStop;
+
+        int stateStart = 0;
+        int stateOver =0;
+
         StringBuilder resultString = new StringBuilder();
-        StringBuilder number = new StringBuilder();
+
         resultString.append(chars[0]);
         if(chars[0] == '$'){
             state = State.record;
@@ -32,7 +39,6 @@ public class Solution {
             char current = chars[i];
 
             if(state==State.none){
-
                 if(current == ' '){
                     state = State.space;
                 }
@@ -41,6 +47,7 @@ public class Solution {
 
                 if(current=='$'){
                     state = State.record;
+                    recordStart = i;
                 }else {
                     state = State.none;
                 }
@@ -48,34 +55,54 @@ public class Solution {
             }else if(state == State.record){
 
                 if(48<=current&&57>=current){
-                    number.append(current);
+                    //number.append(current);
                 }else if(current==' '){
+                    recordStop = i;
                     // record number with discount
-                    if(!number.isEmpty()){
-                        double v = Double.parseDouble(number.toString());
+                    double v = createDoubleFromChars(chars,recordStart,recordStop);
+                    if(v!=-1){
                         resultString.append(String.format("%.2f",v-(v*discount/100)));
-                        number.setLength(0);
-                        state = State.space;
                     }
+
+                    state = State.space;
                     resultString.append(current);
                 }else{
+                    recordStop = i;
                     // record
-                    resultString.append(number);
+                    double v = createDoubleFromChars(chars,recordStart,recordStop);
+                    if(v!=-1)
+                        resultString.append(((int)v));
                     state = State.none;
 
                     resultString.append(current);
-                    number.setLength(0);
                 }
 
             }
         }
 
-        if(state==State.record && !number.isEmpty()){
-            double v = Double.parseDouble(number.toString());
-            resultString.append(String.format("%.2f",v-(v*discount/100)));
+        if(state==State.record){
+            double v = createDoubleFromChars(chars,recordStart,chars.length);
+            if(v!=-1)
+                resultString.append(String.format("%.2f",v-(v*discount/100)));
         }
 
         return resultString.toString();
+    }
+
+
+    private static double createDoubleFromChars(char[] chars,int f, int l){
+
+        if(f+1>=l){
+            return -1;
+        }
+        double num = 0;
+        double m = 1;
+        for (int i = l-1; i >=f+1 ; i--) {
+            num += (chars[i]-48)*m;
+            m*=10;
+        }
+
+        return num;
     }
 
 
