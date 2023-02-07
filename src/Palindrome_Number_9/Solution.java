0507dc39445c9a0d100f6e90f4a8dbace34f64fc
ozantekce <ozantekce@gmail.com>
2023-02-07 16:24:49 +0300

    Update Solution.java

--- src/Palindrome_Number_9/Solution.java
+++ src/Palindrome_Number_9/Solution.java
@@ -9,13 +9,45 @@ public class Solution {
     public static void main(String[] args) {
 
         System.out.println(isPalindrome(-121));
-        System.out.println(isPalindrome2(12321));
+        System.out.println(isPalindrome(1000000001));
 
 
     }
 
 
     public static boolean isPalindrome(int x) {
+
+        if(x<0)
+            return false;
+        // find size
+        int size = 0;
+        long mul = 1;
+        while (true){
+            if(mul>x){
+                break;
+            }
+            size++;
+            mul*=10;
+        }
+        mul/=10;
+        for (int i = 0; i < size/2; i++) {
+            if(x==0)
+                return true;
+            int v1 = (int) (x/mul);
+            int v2 = x%10;
+
+            if(v1!=v2)
+                return false;
+            x -= v1*mul;
+            x/=10;
+            mul/=100;
+        }
+
+        return true;
+    }
+
+
+    public static boolean isPalindrome2(int x) {
 
         if(x<0)
             return false;
@@ -32,7 +64,7 @@ public class Solution {
     }
 
 
-    public static boolean isPalindrome2(int x) {
+    public static boolean isPalindrome3(int x) {
 
         if(x<0)
             return false;
