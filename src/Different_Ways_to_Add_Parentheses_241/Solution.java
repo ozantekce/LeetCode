e70a31edc0ba66ce51230684ccee1e5544e7e44a
ozantekce <ozantekce@gmail.com>
2023-07-05 15:31:24 +0300

    Update Solution.java

--- src/Different_Ways_to_Add_Parentheses_241/Solution.java
+++ src/Different_Ways_to_Add_Parentheses_241/Solution.java
@@ -8,58 +8,68 @@ public class Solution {
     public static void main(String[] args) {
 
         //System.out.println(diffWaysToCompute("55+222-113-1"));
-        System.out.println(diffWaysToCompute("2-1-1"));
+        //System.out.println(diffWaysToCompute("2-1-1"));
         //System.out.println(diffWaysToCompute("22-10-5"));
         System.out.println(diffWaysToCompute("2*3-4*5"));
+        System.out.println(diffWaysToCompute("1-2+3*4-5*6-7+8*9"));
+
 
     }
 
 
-    static HashMap<String,Integer> Tokens = new HashMap<>();
+    private static final HashMap<String,Integer> TokenToInt = new HashMap<>();
+
+    private static final Map<String, String> TokenMap = new HashMap<>();
+    private static final char TokenFirstValue = 58;
+    private static final char TokenLastValue = 255;
+
+    private static final StringBuilder currentValue = new StringBuilder();
 
     public static List<Integer> diffWaysToCompute(String expression) {
 
-
+        TokenMap.clear();
+        TokenToInt.clear();
+        currentValue.setLength(0);
+        currentValue.append(TokenFirstValue);
         HashSet<String> skip = new HashSet<>();
         Queue<String> frontier = new ArrayDeque<>();
         frontier.add(expression);
 
         List<Integer> result = new ArrayList<>();
-
+        StringBuilder stringBuilder = new StringBuilder();
         while (!frontier.isEmpty()){
             String current = frontier.poll();
+
             if(skip.contains(current))continue;
             skip.add(current);
-            //System.out.println("C : "+current);
-            int bracketsControl = 0;
-            int childs = 0;
-            for (int i = 0; i < current.length(); i++) {
-                if(current.charAt(i)=='('){
-                    bracketsControl++;
-                }else if(current.charAt(i)==')'){
-                    bracketsControl--;
-                }
-                if(isOperarion(current.charAt(i)) && bracketsControl==0){
-                    findLeftNumber(current,i);
-                    findRightNumber(current,i);
-                    String left = current.substring(LeftResult[0], LeftResult[1]);
-                    String right = current.substring(RightResult[0], RightResult[1]);
-                    String merge = createToken(current.charAt(i),left,right);
-                    //System.out.println(left+" "+current.charAt(i)+" "+right);
-                    //System.out.println(toInteger(current.charAt(i),left,right));
-                    String next = mergeLeftRight(current,LeftResult[0],RightResult[1]
-                            ,merge);
-                    //System.out.println("next : "+next);
+
+            stringBuilder.setLength(0);
+            stringBuilder.append(current);
+
+            boolean isLeaf = true;
+            for (int i = 0; i < stringBuilder.length(); i++) {
+
+                if(isOperation(stringBuilder.charAt(i))){
+
+                    findLeftNumber(stringBuilder,i);
+                    findRightNumber(stringBuilder,i);
+
+                    String left = stringBuilder.substring(LeftResult[0], LeftResult[1]);
+                    String right = stringBuilder.substring(RightResult[0], RightResult[1]);
+                    String token = getToken(stringBuilder.charAt(i),left,right);
+
+                    String next = createNextExpression(stringBuilder,LeftResult[0],RightResult[1]
+                            ,token);
+
 
                     frontier.add(next);
-                    childs++;
+                    isLeaf = false;
                 }
             }
-            if(childs==0){
+            if(isLeaf){
                 result.add(tokenToInt(current));
             }
         }
-
 
 
         return result;
@@ -67,22 +77,17 @@ public class Solution {
 
 
     public static int[] LeftResult = new int[2];
-    public static void findLeftNumber(String str, int opIndex){
+    public static void findLeftNumber(StringBuilder str, int opIndex){
 
         int start;
         int end = opIndex;
         int i = end-1;
-        int bracketsControl = 0;
         for (; i >=0 ; i--) {
-            if(str.charAt(i)=='('){
-                bracketsControl++;
-            }else if(str.charAt(i)==')'){
-                bracketsControl--;
-            }
-            if(isOperarion(str.charAt(i)) && bracketsControl == 0){
+            if(isOperation(str.charAt(i))){
                 break;
             }
         }
+
         start = i+1;
         LeftResult[0] = start;
         LeftResult[1] = end;
@@ -90,19 +95,13 @@ public class Solution {
 
 
     public static int[] RightResult = new int[2];
-    public static void findRightNumber(String str, int opIndex){
+    public static void findRightNumber(StringBuilder str, int opIndex){
 
         int start = opIndex+1;
         int end;
         int i = start;
-        int bracketsControl = 0;
         for (; i < str.length() ; i++) {
-            if(str.charAt(i)=='('){
-                bracketsControl++;
-            }else if(str.charAt(i)==')'){
-                bracketsControl--;
-            }
-            if(isOperarion(str.charAt(i))&& bracketsControl == 0){
+            if(isOperation(str.charAt(i))){
                 break;
             }
         }
@@ -112,9 +111,49 @@ public class Solution {
     }
 
 
-    public static String createToken(char op, String left, String right){
+    public static String createToken(String expression) {
+        if (TokenMap.containsKey(expression)) {
+            return TokenMap.get(expression);
+        } else {
+            incrementTokenValue();
+            String cv = currentValue.toString();
+            TokenMap.put(expression, cv);
+            return cv;
+        }
+    }
 
-        String token = "("+left+op+right+")";
+    private static void incrementTokenValue() {
+
+        int i = currentValue.length() - 1;
+        while (i >= 0) {
+            if (currentValue.charAt(i) != TokenLastValue) {
+                currentValue.setCharAt(i, (char) (currentValue.charAt(i) + 1));
+                break;
+            } else {
+                currentValue.setCharAt(i, TokenFirstValue);
+                i--;
+            }
+        }
+        if (i < 0) {
+            currentValue.insert(0, TokenFirstValue);
+        }
+
+    }
+
+    private static final StringBuilder rawTokenBuilder = new StringBuilder();
+    public static String getToken(char op, String left, String right){
+
+        rawTokenBuilder.setLength(0);
+        rawTokenBuilder.append("(");
+        rawTokenBuilder.append(left);
+        rawTokenBuilder.append(op);
+        rawTokenBuilder.append(right);
+        rawTokenBuilder.append(")");
+
+        String token = createToken(rawTokenBuilder.toString());
+        if(TokenToInt.containsKey(token)){
+            return token;
+        }
         int valueOfToken = 0;
         int leftValue = tokenToInt(left);
         int rightValue = tokenToInt(right);
@@ -122,27 +161,28 @@ public class Solution {
         else if(op=='/') valueOfToken = leftValue/rightValue;
         else if(op=='+') valueOfToken = leftValue+rightValue;
         else if(op=='-') valueOfToken = leftValue-rightValue;
-        Tokens.put(token,valueOfToken);
+        TokenToInt.put(token,valueOfToken);
         return token;
     }
 
     public static int tokenToInt(String token){
-        if(!Tokens.containsKey(token)) return Integer.parseInt(token);
-        return Tokens.get(token);
+        if(!TokenToInt.containsKey(token)){
+            TokenToInt.put(token,Integer.parseInt(token));
+        }
+        return TokenToInt.get(token);
     }
 
 
-
-    public static String mergeLeftRight(String str, int start, int end, String value){
-        String prefix = str.substring(0,start);
-        String posix = "";
-        posix = str.substring(end,str.length());
-        String result = prefix + value + posix;
-
-        return result;
+    private static StringBuilder resultBuilder = new StringBuilder();
+    public static String createNextExpression(StringBuilder str, int start, int end, String token){
+        resultBuilder.setLength(0);
+        resultBuilder.append(str, 0, start);
+        resultBuilder.append(token);
+        resultBuilder.append(str, end, str.length());
+        return resultBuilder.toString();
     }
 
-    public static boolean isOperarion(char c){
+    public static boolean isOperation(char c){
         return c == '*' || c== '/' || c == '+' || c=='-';
     }
 
