4cfdc578f84b048f6051276f67fae0c3fd73154b
ozantekce <ozantekce@gmail.com>
2023-07-07 03:52:02 +0300

    125. Valid Palindrome

--- src/Valid_Palindrome_125/Solution.java
+++ src/Valid_Palindrome_125/Solution.java
@@ -5,6 +5,9 @@ public class Solution {
     public static void main(String[] args) {
 
         System.out.println(isPalindrome("0P"));
+        //System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        //System.out.println(isPalindrome("race a car"));
+        //System.out.println(isPalindrome(" "));
 
     }
 
@@ -12,13 +15,34 @@ public class Solution {
 
     public static boolean isPalindrome(String s) {
 
-        s = s.toLowerCase();
-        s = s.replaceAll("((?![a-z0-9]).)*","");
-        StringBuilder s2 = new StringBuilder(s);
-        s2.reverse();
-        //System.out.println(s);
-        //System.out.println(s2);
-        return s.equals(s2.toString());
+        // create valid string
+        StringBuilder validString = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if((c>=97&&c<=122) || (c>=48&&c<=57)){
+                validString.append(c);
+            }else if(c>=65&&c<=90){
+                c+=32;
+                validString.append(c);
+            }
+        }
+        System.out.println(validString);
+        //check
+        boolean isPalindrome = true;
+        int first = 0, last = validString.length() - 1;
+        while (first < last) {
+            if (validString.charAt(first) != validString.charAt(last)) {
+                isPalindrome =  false;
+                break;
+            }
+            first++;
+            last--;
+        }
+        return isPalindrome;
     }
+
+
+
 
 }
