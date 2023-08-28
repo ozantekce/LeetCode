36441b27542e670c2a2512628da2f845525f4b95
ozantekce <ozantekce@gmail.com>
2023-08-28 23:29:52 +0300

    Update Solution.java

--- src/Restore_IP_Addresses_93/Solution.java
+++ src/Restore_IP_Addresses_93/Solution.java
@@ -20,46 +20,48 @@ public class Solution {
 
 
     public static List<String> restoreIpAddresses(String s) {
-        result.clear();
-        //counter = 0;
-        findRecursive(1,new StringBuilder().append(s.charAt(0)),charToInt(s.charAt(0)),0,s.toCharArray(),false);
-        //System.out.println(counter);
-        return result;
+        chars = s.toCharArray();
+        results.clear();
+        findRecursive(1,new StringBuilder().append(chars[0]), getIntValue(0),0,false);
+        return results;
     }
 
-    private static List<String> result = new ArrayList<>();
-    //private static int counter = 0;
-    private static void findRecursive(int index, StringBuilder stringBuilder,int currentValue,int numberOfDots, char[] chars,boolean dotAdded){
+    private static final List<String> results = new ArrayList<>();
+    private static char[] chars;
+
+    private static void findRecursive(
+            int index
+            ,StringBuilder stringBuilder
+            ,int currentValue
+            ,int numberOfDots
+            ,boolean dotAdded){
 
         if(index>=chars.length || currentValue>255) return;
 
         if(numberOfDots == 3){
             int sizeOfRemainderPart = chars.length - index;
-            if(sizeOfRemainderPart<=0) return;
+
+            if(sizeOfRemainderPart<=0 || sizeOfRemainderPart>3) return;
+
             if(sizeOfRemainderPart==1){
-                //counter++;
-                StringBuilder s = new StringBuilder(stringBuilder.toString());
-                for (int i = index; i <chars.length ; i++) {
-                    s.append(chars[i]);
-                }
-                //System.out.println(s);
-                result.add(s.toString());
-            }else if(sizeOfRemainderPart<=3){
-                if(charToInt(chars[index])==0)return;
+                stringBuilder.append(chars[index]);
+                results.add(stringBuilder.toString());
+                stringBuilder.deleteCharAt(stringBuilder.length()-1);
+
+            }else{
+
+                if(getIntValue(index)==0)return;
+
                 int val = 0;
                 for (int i = index; i < chars.length; i++) {
-                    val += charToInt(chars[i]);
+                    val += getIntValue(i);
                     val *= 10;
                 }
-                val/=10;
-                if(val<=255){
-                    //counter++;
-                    StringBuilder s = new StringBuilder(stringBuilder.toString());
-                    for (int i = index; i <chars.length ; i++) {
-                        s.append(chars[i]);
-                    }
-                    //System.out.println(s);
-                    result.add(s.toString());
+
+                if(val<=2550){
+                    for (int i = index; i <chars.length ; i++) stringBuilder.append(chars[i]);
+                    results.add(stringBuilder.toString());
+                    for (int i = index; i <chars.length ; i++) stringBuilder.deleteCharAt(stringBuilder.length()-1);
                 }
             }
 
@@ -68,17 +70,16 @@ public class Solution {
 
         //addDot
         if(!dotAdded){
-            findRecursive(index,stringBuilder.append('.'),0,numberOfDots+1,chars,true);
+            findRecursive(index,stringBuilder.append('.'),0,numberOfDots+1,true);
             stringBuilder.deleteCharAt(stringBuilder.length()-1);
         }
 
         //increaseVal
+        if(!dotAdded && currentValue == 0)return;
 
-        if(!dotAdded){
-            if(currentValue==0)return;
-        }
+        findRecursive(index+1,stringBuilder.append(chars[index])
+                ,currentValue*10+ getIntValue(index),numberOfDots,false);
 
-        findRecursive(index+1,stringBuilder.append(chars[index]),currentValue*10+charToInt(chars[index]),numberOfDots,chars,false);
         stringBuilder.deleteCharAt(stringBuilder.length()-1);
 
     }
@@ -86,8 +87,7 @@ public class Solution {
 
 
 
-    private static int charToInt(char c) { return c -'0';}
-
+    private static int getIntValue(int index) { return chars[index] -'0';}
 
 
 }
