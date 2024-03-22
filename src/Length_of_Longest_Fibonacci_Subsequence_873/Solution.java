16a519978f36c0366f576cdb5e7cf8e305a4c746
ozantekce <ozantekce@gmail.com>
2024-03-23 01:40:42 +0300

    Update Solution.java

--- src/Length_of_Longest_Fibonacci_Subsequence_873/Solution.java
+++ src/Length_of_Longest_Fibonacci_Subsequence_873/Solution.java
@@ -1,4 +1,5 @@
 package Length_of_Longest_Fibonacci_Subsequence_873;
+import java.util.HashMap;
 import java.util.HashSet;
 
 
@@ -10,47 +11,40 @@ public class Solution {
 
         System.out.println(lenLongestFibSubseq(new int[]{1,2,3,4,5,6,7,8}));
         System.out.println(lenLongestFibSubseq(new int[]{1,3,7,11,12,14,18}));
+        //System.out.println(lenLongestFibSubseq(new int[]{1,3,7,11,12,14,18}));
 
     }
 
 
 
 
-    private static final HashSet<Integer> Set = new HashSet<>();
     public static int lenLongestFibSubseq(int[] arr) {
-        Set.clear();
-        for (int i = 0; i < arr.length; i++) {
-            Set.add(arr[i]);
-        }
-        int maxCount = 0;
-        for (int i = 0; i < arr.length-2; i++) {
 
-            for (int j = i+1; j < arr.length-1; j++) {
-                int first = arr[i];
-                int second = arr[j];
-                int third = first + second;
-                if(Set.contains(third)){
-                    int count = 3;
-                    int last0 = second;
-                    int last1 = third;
-                    while (true){
-                        int next = last0 + last1;
-                        if(Set.contains(next)){
-                            count++;
-                            last0 = last1;
-                            last1 = next;
-                        }else{
-                            break;
-                        }
-                    }
-                    maxCount = Math.max(maxCount, count);
+        HashMap<Integer,Integer> valueToIndex = new HashMap<>(arr.length);
+
+        int maxValue = 0;
+        for (int i = 0; i < arr.length; i++) {
+            valueToIndex.put(arr[i], i);
+            maxValue = Math.max(maxValue, arr[i]);
+        }
+
+        int [][] dp = new int[arr.length][arr.length];
+
+        int max = 0;
+        for (int i = 0; i < dp.length-2; i++) {
+            for (int j = i+1; j < dp.length-1; j++) {
+                int nextValue = arr[i] + arr[j];
+                if(nextValue > maxValue){
+                    break;
+                }
+                if(valueToIndex.containsKey(nextValue)){
+                    int val = dp[j][valueToIndex.get(nextValue)] = dp[i][j] + 1;
+                    max = Math.max(val, max);
                 }
             }
         }
 
-
-        return maxCount;
+        return (max < 1) ? 0 : max+2;
     }
-
 
 }
