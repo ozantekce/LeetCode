4f5096743a58d1d7296ffdcd7504a7b19a7d2f05
ozantekce <ozantekce@gmail.com>
2024-03-23 07:21:31 +0300

    1775. Equal Sum Arrays With Minimum Number of Operations
    Beats 80.62%

--- src/Equal_Sum_Arrays_With_Minimum_Number_of_Operations_1775/Solution.java
+++ src/Equal_Sum_Arrays_With_Minimum_Number_of_Operations_1775/Solution.java
@@ -17,86 +17,76 @@ public class Solution {
 
 
     public static int minOperations(int[] nums1, int[] nums2) {
+        int total1 = 0, total2 = 0;
+        int[] count1 = new int[6];
+        int[] count2 = new int[6];
 
-        int total1 = 0 , total2 = 0;
-        HashMap<Integer, Integer> counter1 = new HashMap<>(6);
-        HashMap<Integer, Integer> counter2 = new HashMap<>(6);
-
-        for (int num : nums1){
+        for (int num : nums1) {
             total1 += num;
-            counter1.put(num, counter1.getOrDefault(num, 0)+1);
+            count1[num - 1]++;
         }
-        for (int num : nums2){
+        for (int num : nums2) {
             total2 += num;
-            counter2.put(num, counter2.getOrDefault(num, 0)+1);
+            count2[num - 1]++;
         }
 
-        if(total1 == total2) return 0;
+        if (total1 == total2) return 0;
 
-        // swap
-        if(total2 > total1){
-            int tempInt = total1;
+        // Swap logic if necessary
+        if (total2 > total1) {
+            int tempTotal = total1;
             total1 = total2;
-            total2 = tempInt;
+            total2 = tempTotal;
 
-            HashMap<Integer,Integer> tempMap = counter1;
-            counter1 = counter2;
-            counter2 = tempMap;
+            int[] tempCount = count1;
+            count1 = count2;
+            count2 = tempCount;
         }
 
         int diff = total1 - total2;
-
-        int i_start = 1;
-        int j_start = 6;
-
+        int i_start = 0;
+        int j_start = 5;
         int ops = 0;
-        while (true){
-            if(diff > 0){
-                ops++;
-                int smaller = 6;
-                for (int i = i_start; i <= 6; i++) {
-                    if(counter2.containsKey(i) && counter2.get(i) > 0){
-                        smaller = i;
-                        i_start = smaller;
-                        break;
-                    }
+
+        while (diff > 0) {
+            ops++;
+            int smaller = 6;
+            for (int i = i_start; i < 6; i++) {
+                if (count2[i] > 0) {
+                    smaller = i + 1;
+                    i_start = i;
+                    break;
                 }
-                int bigger = 1;
-                for (int i = j_start; i >= 1; i--) {
-                    if(counter1.containsKey(i) && counter1.get(i) > 0){
-                        bigger = i;
-                        j_start = bigger;
-                        break;
-                    }
+            }
+            int bigger = 1;
+            for (int j = j_start; j >= 0; j--) {
+                if (count1[j] > 0) {
+                    bigger = j + 1;
+                    j_start = j;
+                    break;
                 }
+            }
 
-                if(smaller == 6 && bigger == 1)
-                    return -1;
+            if (smaller == 6 && bigger == 1) return -1;
 
-                int d1 = 6 - smaller;
-                int d2 = bigger - 1;
+            int d1 = 6 - smaller;
+            int d2 = bigger - 1;
 
-                if(d2 > d1){
-                    int dec = bigger - 1;
-                    diff -= dec;
-
-                    counter1.put(bigger, counter1.get(bigger)-1);
-                    counter1.put(1, counter1.getOrDefault(1, 0)+1);
-
-                }else {
-                    int inc = 6 - smaller;
-                    diff -= inc;
-
-                    counter2.put(smaller, counter2.get(smaller)-1);
-                    counter2.put(6, counter2.getOrDefault(6, 0)+1);
-                }
-
-            }else{
-                return ops;
+            if (d2 > d1) {
+                int dec = bigger - 1;
+                diff -= dec;
+                count1[bigger - 1]--;
+                count1[0]++;
+            } else {
+                int inc = 6 - smaller;
+                diff -= inc;
+                count2[smaller - 1]--;
+                count2[5]++;
             }
         }
-
+        return ops;
     }
+
 
 
 
