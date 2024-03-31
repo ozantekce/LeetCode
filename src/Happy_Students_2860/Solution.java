d0df60a4a2221666d532d9644dac347ffae9b00a
ozantekce <ozantekce@gmail.com>
2024-03-31 17:17:05 +0300

    2860. Happy Students
    Beats 94.23%

--- src/Happy_Students_2860/Solution.java
+++ src/Happy_Students_2860/Solution.java
@@ -16,25 +16,25 @@ public class Solution {
 
     public static int countWays(List<Integer> nums) {
 
+        // selectedCount > Max(selected0, selected1,...)
+        // selectedCount < Min(nonselected0, nonselected1, ....)
+
         int counter = 0;
         int [] array = countingSort(nums);
-        int n = array.length;
+        int n = array.length-1;
         int selectedCount = 0;
         int maxSelected = -1;
         int minNonselected = array[0];
 
-        for (int i = 0; i < array.length; i++) {
-            if(meetConditions(selectedCount, maxSelected, minNonselected, n)){
+        for (int i = 0; i < n; i++) {
+            if(meetConditions(selectedCount, maxSelected, minNonselected)){
                 counter++;
             }
             selectedCount++;
             maxSelected = array[selectedCount - 1];
-            if(selectedCount < n)
-                minNonselected = array[selectedCount];
-            else
-                minNonselected = 0;
+            minNonselected = array[selectedCount];
         }
-        if(meetConditions(selectedCount, maxSelected, minNonselected, n)){
+        if(meetConditions(selectedCount, maxSelected, minNonselected)){
             counter++;
         }
 
@@ -47,30 +47,28 @@ public class Solution {
             return new int[0];
         }
 
-        int[] counter = new int[nums.size()];
-        int[] sortedArray = new int[nums.size()];
+        int[] counter = new int[nums.size()+1];
+        int[] sortedArray = new int[nums.size()+1];
 
         for (int num : nums) {
             counter[num]++;
         }
-
-        for (int i = 1; i < counter.length; i++) {
-            counter[i] += counter[i - 1];
+        int i = 0;
+        int pointer = 0;
+        while (pointer < counter.length - 1){
+            int addCount = counter[pointer];
+            for (int j = 0; j < addCount; j++) {
+                sortedArray[i++] = pointer;
+            }
+            pointer++;
         }
-
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            sortedArray[counter[nums.get(i)] - 1] = nums.get(i);
-            counter[nums.get(i)]--;
-        }
-
+        sortedArray[i] = nums.size() + 1;
         return sortedArray;
     }
 
 
-    private static  boolean meetConditions(int selectedCount, int maxSelected, int minNonselected, int n){
-        if(selectedCount == 0) return  selectedCount < minNonselected;
-        else if(selectedCount == n)  return selectedCount > maxSelected;
-        else return selectedCount > maxSelected && selectedCount < minNonselected;
+    private static  boolean meetConditions(int selectedCount, int maxSelected, int minNonselected){
+        return selectedCount > maxSelected && selectedCount < minNonselected;
     }
 
 
