9de78fad358ed9228ef119b204776632d95bd14c
ozantekce <ozantekce@gmail.com>
2024-04-01 15:19:04 +0300

    46. Permutations

--- src/Permutations_46/Solution.java
+++ src/Permutations_46/Solution.java
@@ -1,7 +1,6 @@
 package Permutations_46;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class Solution {
@@ -17,24 +16,60 @@ public class Solution {
     public static List<List<Integer>> permute(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
-        backtracking(nums, result, new ArrayList<>(nums.length), new HashSet<>(nums.length));
+        backtracking(nums, result, new MyList(nums.length));
         return result;
     }
 
-    private static void backtracking(int[] nums, List<List<Integer>> result, List<Integer> current, HashSet<Integer> skip){
+    private static void backtracking(int[] nums, List<List<Integer>> result, MyList current){
 
-        if(skip.size() == nums.length){
-            result.add(new ArrayList<>(current));
+        if(current.size() == nums.length){
+            result.add(current.getAsList());
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if(!skip.contains(i)){
-                current.add(nums[i]);
-                skip.add(i);
-                backtracking(nums,result,current,skip);
-                current.remove((Object)nums[i]);
-                skip.remove(i);
+            if(current.contains(nums[i]))
+                continue;
+            current.add(nums[i]);
+            backtracking(nums,result,current);
+            current.removeLast();
+        }
+
+    }
+
+    private static class MyList{
+        private boolean set [];
+        private int nums [];
+        private int size;
+
+        public MyList(int capacity){
+            nums = new int[capacity];
+            size = 0;
+            set = new boolean[22];
+        }
+
+        public void add(int num){
+            nums[size++] = num;
+            set[num+10] = true;
+        }
+        public void removeLast(){
+            set[nums[size-1]+10] = false;
+            size--;
+        }
+
+        public List<Integer> getAsList() {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                list.add(nums[i]);
             }
+            return list;
+        }
+
+        public int size(){
+            return size;
+        }
+
+        public boolean contains(int num){
+            return set[num+10];
         }
 
     }
