df5a93668450968d6c8e9ae8c7a9ecc004d0295c
ozantekce <ozantekce@gmail.com>
2024-04-03 03:19:03 +0300

    1140. Stone Game II

--- src/Stone_Game_II_1140/Solution.java
+++ src/Stone_Game_II_1140/Solution.java
@@ -6,11 +6,11 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(stoneGameII(new int[]{2,7,9,4,4}));
+        //System.out.println(stoneGameII(new int[]{2,7,9,4,4}));
         System.out.println(stoneGameII(new int[]{1,2,3,4,5,100}));
-        System.out.println(stoneGameII(new int[]{1}));
-        System.out.println(stoneGameII(new int[]{1,1,1}));
-        System.out.println(stoneGameII(new int[]{3111,4303,2722,2183,6351,5227,8964,7167,9286,6626,2347,1465,5201,7240,5463,8523,8163,9391,8616,5063,7837,7050,1246,9579,7744,6932,7704,9841,6163,4829,7324,6006,4689,8781,621}));
+        //System.out.println(stoneGameII(new int[]{1}));
+        //System.out.println(stoneGameII(new int[]{1,1,1}));
+        //System.out.println(stoneGameII(new int[]{3111,4303,2722,2183,6351,5227,8964,7167,9286,6626,2347,1465,5201,7240,5463,8523,8163,9391,8616,5063,7837,7050,1246,9579,7744,6932,7704,9841,6163,4829,7324,6006,4689,8781,621}));
 
     }
 
@@ -18,7 +18,12 @@ public class Solution {
     private static int Memory [][][];
 
     public static int stoneGameII(int[] piles) {
-        Memory = new int[piles.length+1][piles.length+1][2];
+        Memory = new int[2][piles.length+1][piles.length];
+        for (int i = 0; i < Memory.length; i++) {
+            for (int j = 0; j < Memory[0].length; j++) {
+                Arrays.fill(Memory[i][j], -1);
+            }
+        }
         int a = recursive(piles, 0, 1, true);
         return a;
     }
@@ -26,14 +31,12 @@ public class Solution {
 
 
     private static int recursive(int [] nums, int i, int m, boolean turn){
-
         if(i >= nums.length){
             return 0;
         }
-
         if(turn){
-            if(Memory[i][m][1] != 0)
-                return Memory[i][m][1];
+            if(Memory[1][m][i] != -1)
+                return Memory[1][m][i];
 
             int max = Integer.MIN_VALUE;
             int c = 0;
@@ -44,11 +47,11 @@ public class Solution {
                 c += nums[i+j-1];
                 max = Math.max(max, c + recursive(nums, i + j, Math.max(m, j), false));
             }
-            Memory[i][m][1] = max;
+            Memory[1][m][i] = max;
             return max;
         }else{
-            if(Memory[i][m][0] != 0)
-                return Memory[i][m][0];
+            if(Memory[0][m][i] != -1)
+                return Memory[0][m][i];
 
             int min = Integer.MAX_VALUE;
             for (int j = 1; j <= 2 * m; j++) {
@@ -57,12 +60,11 @@ public class Solution {
                 }
                 min = Math.min(min, recursive(nums,i + j, Math.max(m, j), true));
             }
-            Memory[i][m][0] = min;
+            Memory[0][m][i] = min;
             return min;
         }
 
     }
-
 
 
 }
