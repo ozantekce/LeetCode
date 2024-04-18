cf1695a2cf3366854794f9259c3b6b09224ae87d
ozantekce <ozantekce@gmail.com>
2024-04-19 02:29:02 +0300

    2469. Convert the Temperature

---
+++ src/Convert_the_Temperature_2469/Solution.java
@@ -0,0 +1,17 @@
+package Convert_the_Temperature_2469;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public double[] convertTemperature(double celsius) {
+        return new double[]{ celsius+273.15, celsius * 1.80 + 32.00 };
+    }
+
+
+}
