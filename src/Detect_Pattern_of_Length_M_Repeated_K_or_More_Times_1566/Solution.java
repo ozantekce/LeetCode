dcc3e438a76cff02e80779fd8a52c592877168e8
ozantekce <ozantekce@gmail.com>
2024-04-18 06:39:00 +0300

    1566. Detect Pattern of Length M Repeated K or More Times

---
+++ src/Detect_Pattern_of_Length_M_Repeated_K_or_More_Times_1566/Solution.java
@@ -0,0 +1,51 @@
+package Detect_Pattern_of_Length_M_Repeated_K_or_More_Times_1566;
+
+import java.util.Arrays;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        //System.out.println(containsPattern(new int[]{1,2,4,4,4,4}, 1, 3));
+        //System.out.println(containsPattern(new int[]{1,2,1,2,1,1,1,3}, 2, 2));
+        //System.out.println(containsPattern(new int[]{1,2,1,2,1,3}, 2, 3));
+        //System.out.println(containsPattern(new int[]{2,2,2,2}, 2, 3));
+        //System.out.println(containsPattern(new int[]{1,2,3,1,2}, 2, 2));
+        System.out.println(containsPattern(new int[]{2,2,1,2,2,1,1,1,2,1}, 2, 2));
+    }
+
+    public static boolean containsPattern(int[] arr, int m, int k) {
+
+
+        int [] window = new int[m];
+
+        for (int i = 0; i < arr.length - m; i++) {
+            System.arraycopy(arr, i, window, 0, m);
+            int f = 0;
+            int c_k = k - 1;
+            for (int j = i + m; j < arr.length; j++) {
+
+                if(arr[j] != window[f]){
+                    break;
+                }
+                f++;
+                if(f == m){
+                    f = 0;
+                    c_k--;
+                    if(c_k == 0){
+                        return true;
+                    }
+                }
+
+            }
+
+        }
+
+
+
+        return false;
+    }
+
+
+}
