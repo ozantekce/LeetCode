9d60311a037802fc1bdfd3af49ff3d6622271c3a
ozantekce <ozantekce@gmail.com>
2024-04-19 01:38:42 +0300

    2769. Find the Maximum Achievable Number

---
+++ src/Find_the_Maximum_Achievable_Number_2769/Solution.java
@@ -0,0 +1,17 @@
+package Find_the_Maximum_Achievable_Number_2769;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public int theMaximumAchievableX(int num, int t) {
+        return 2*t + num;
+    }
+
+
+}
