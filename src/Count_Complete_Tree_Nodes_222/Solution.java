ab8c20527ca9f7541720a4e008f91d015fdeecda
ozantekce <ozantekce@gmail.com>
2024-04-21 13:15:25 +0300

    222. Count Complete Tree Nodes

---
+++ src/Count_Complete_Tree_Nodes_222/Solution.java
@@ -0,0 +1,54 @@
+package Count_Complete_Tree_Nodes_222;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        root.right.left = new TreeNode(6);
+
+        System.out.println(countNodes(root));
+
+    }
+
+
+    public static int countNodes(TreeNode root) {
+
+        if(root == null)
+            return 0;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+
+}
