bc1bd00ae92bf6c7bf0f2fb2586d15242c350d9f
ozantekce <ozantekce@gmail.com>
2024-04-26 13:45:06 +0300

    1289. Minimum Falling Path Sum II

--- src/Minimum_Falling_Path_Sum_II_1289/Solution.java
+++ src/Minimum_Falling_Path_Sum_II_1289/Solution.java
@@ -1,5 +1,7 @@
 package Minimum_Falling_Path_Sum_II_1289;
 
+import java.util.Arrays;
+
 public class Solution {
 
 
@@ -11,8 +13,43 @@ public class Solution {
 
     }
 
-    private static int [][] memory;
+
     public static int minFallingPathSum(int[][] grid) {
+
+        int n = grid.length;
+        int [][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = grid[i][j];
+            }
+        }
+
+        for (int row = 1; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                int min = Integer.MAX_VALUE;
+                for (int i = 0; i < n; i++) {
+                    if(i == col)
+                        continue;
+                    min = Math.min(min, dp[row][col] + dp[row-1][i]);
+                }
+                min = min == Integer.MAX_VALUE ? dp[row][col] : min;
+                dp[row][col] = min;
+            }
+
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(dp[n-1][i], min);
+            //System.out.println(Arrays.toString(dp[i]));
+        }
+
+        return min;
+    }
+
+    private static int [][] memory;
+    public static int minFallingPathSumRecursive(int[][] grid) {
 
         memory = new int[grid.length][grid.length];
         int min = Integer.MAX_VALUE;
@@ -22,9 +59,6 @@ public class Solution {
 
         return min;
     }
-
-    
-
     private static int recursive(int[][] grid, int row, int col){
 
         if(row == grid.length)
