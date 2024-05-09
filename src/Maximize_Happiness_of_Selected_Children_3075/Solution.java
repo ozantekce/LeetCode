5cadce37002ad2a91cfa518415613ec42bf9d5a6
ozantekce <ozantekce@gmail.com>
2024-05-09 17:56:41 +0300

    3075. Maximize Happiness of Selected Children

--- src/Maximize_Happiness_of_Selected_Children_3075/Solution.java
+++ src/Maximize_Happiness_of_Selected_Children_3075/Solution.java
@@ -1,6 +1,8 @@
 package Maximize_Happiness_of_Selected_Children_3075;
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Solution {
 
@@ -10,18 +12,18 @@ public class Solution {
         System.out.println(maximumHappinessSum(new int[]{1,2,3},2));
         System.out.println(maximumHappinessSum(new int[]{1,1,1,1},2));
         System.out.println(maximumHappinessSum(new int[]{2,3,4,5},1));
+        System.out.println(maximumHappinessSum(new int[]{2,2,10,1,1,1,2,3,1,1},4));
 
     }
-
 
     public static long maximumHappinessSum(int[] happiness, int k) {
 
         int n = happiness.length;
         long res = 0;
-        Arrays.sort(happiness);
-
-        for (int i = 0; i < k && i< happiness.length; i++) {
-            int val = happiness[n - 1 - i] - i;
+        int min = Select(happiness, 0, n-1,n+1-k);
+        Arrays.sort(happiness, min, n);
+        for (int i = min, j = 0; i < n; i++, j++) {
+            int val = happiness[n - 1 - j] - j;
             if(val <= 0)
                 break;
             res += val;
@@ -30,5 +32,65 @@ public class Solution {
         return res;
     }
 
+    public static int Select(int A[],int p,int r,int i){
+
+        if(p==r){
+            return p;
+        }
+
+        int q = R_PartitionH(A,p,r);
+        int k = q-p+1;
+
+        if(i<=k)
+            return Select(A,p,q,i);
+        else
+            return Select(A,q+1,r,i-k);
+
+
+    }
+
+    public static Random random = new Random();
+    public static int R_PartitionH(int A[],int p, int r){
+
+
+        int s = random.nextInt(p,r+1);
+        int holder = A[p];
+        A[p] = A[s];
+        A[s] = holder;
+        return H_Partition(A,p,r);
+
+    }
+
+    private static int H_Partition(int[] A, int p, int r) {
+
+        int pivot = A[p] ;
+        int i=p-1;
+        int j=r+1;
+
+        while (true){
+
+            while (true){
+                j = j-1;
+                if(A[j]<=pivot)
+                    break;
+            }
+
+            while (true){
+                i = i +1;
+                if(A[i]>=pivot)
+                    break;
+            }
+
+            if(i<j){
+                int holder = A[i];
+                A[i] = A[j];
+                A[j] = holder;
+            }else{
+                return j;
+            }
+
+        }
+
+    }
 
 }
