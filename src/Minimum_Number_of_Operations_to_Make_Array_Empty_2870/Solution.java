886e4f0debde07a2c7183e7c48d2757a1c9eb055
ozantekce <ozantekce@gmail.com>
2024-05-14 19:13:42 +0300

    2870. Minimum Number of Operations to Make Array Empty

--- src/Minimum_Number_of_Operations_to_Make_Array_Empty_2870/Solution.java
+++ src/Minimum_Number_of_Operations_to_Make_Array_Empty_2870/Solution.java
@@ -1,5 +1,6 @@
 package Minimum_Number_of_Operations_to_Make_Array_Empty_2870;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Solution {
@@ -9,6 +10,7 @@ public class Solution {
 
         System.out.println(minOperations(new int[]{2,3,3,2,2,4,2,3,4}));
         System.out.println(minOperations(new int[]{2,1,2,2,3,3}));
+        System.out.println(minOperations(new int[]{14,12,14,14,12,14,14,12,12,12,12,14,14,12,14,14,14,12,12}));
 
     }
 
@@ -18,42 +20,21 @@ public class Solution {
 
         HashMap<Integer, Integer> counter = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            counter.put(nums[i], counter.getOrDefault(nums[i],0)+1);
-        }
-        int res = 0;
-        for (var pair : counter.entrySet()) {
-            int count = pair.getValue();
-            int moves = isPossible(count);
-            if(moves == -1)
-                return -1;
-            res += moves;
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
         }
 
+        int res = 0;
+        for (int count : counter.values()) {
+            if(count == 1) return -1;
+            res += (count + 2) / 3;
+        }
         return res;
     }
 
 
-    private static int isPossible(int count){
 
-        if(count < 2)
-            return -1;
-        else if(count == 3)
-            return 1;
-        else if(count == 2)
-            return 1;
 
-        int res = isPossible(count - 3);
-        if(res == -1){
-            res = isPossible(count - 2);
-        }
-        if(res != -1){
-            return res + 1;
-        }else{
-            return -1;
-        }
-
-    }
 
 
 
