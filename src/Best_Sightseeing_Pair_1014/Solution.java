df2fd8c407308c0d32b42db18de5affcdce1f03d
ozantekce <ozantekce@gmail.com>
2024-12-30 07:55:21 +0300

    1014. Best Sightseeing Pair

--- src/Best_Sightseeing_Pair_1014/Solution.java
+++ src/Best_Sightseeing_Pair_1014/Solution.java
@@ -14,18 +14,11 @@ public class Solution {
 
     public static int maxScoreSightseeingPair(int[] values) {
 
-        int [] bestSecondParts = new int[values.length];
-
-        bestSecondParts[values.length-1] = values[values.length-1] - (values.length-1);
-        for (int i = values.length-2; i >= 1; i--) {
-            bestSecondParts[i] = Math.max(bestSecondParts[i+1], values[i] - i);
-        }
-
         int result = 0;
-        for (int i = 0; i < values.length-1; i++) {
-            int firstPart = values[i] + i;
-            int total = firstPart + bestSecondParts[i+1];
-            result = Math.max(total, result);
+        int bestSecondPart = values[values.length-1] - values.length + 1;
+        for (int i = values.length-2; i >= 0; i--) {
+            result = Math.max(result, values[i] + i + bestSecondPart);
+            bestSecondPart = Math.max(bestSecondPart, values[i] - i);
         }
 
         return result;
