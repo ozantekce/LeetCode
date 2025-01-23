0752a10fca36a89cfb08428b3b5be11348243fae
ozantekce <ozantekce@gmail.com>
2025-01-23 22:58:00 +0300

    1765. Map of Highest Peak

--- src/Map_of_Highest_Peak_1765/Solution.java
+++ src/Map_of_Highest_Peak_1765/Solution.java
@@ -1,8 +1,6 @@
 package Map_of_Highest_Peak_1765;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
@@ -20,93 +18,54 @@ public class Solution {
     public static int[][] highestPeak(int[][] isWater) {
 
         int[][] heights = new int[isWater.length][isWater[0].length];
-        List<Pair> heightsN = new ArrayList<>();
+        Queue<int[]> frontier = new ArrayDeque<>();
 
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
                 heights[i][j] = -1;
                 if(isWater[i][j]==1){
                     heights[i][j] = 0;
-                    heightsN.add(new Pair(i,j));
+                    frontier.add(new int[]{i,j});
                 }
             }
         }
 
-        int currentHeight = 0;
-        List<Pair> nextHeights = new ArrayList<>();
-        while (true){
+        while (!frontier.isEmpty()){
+            int[] current = frontier.poll();
+            int currentHeight = heights[current[0]][current[1]];
+            int upI = current[0]-1, upJ = current[1];
+            int downI = current[0]+1, downJ = current[1];
+            int leftI = current[0], leftJ = current[1]-1;
+            int rightI = current[0] ,rightJ = current[1]+1;
 
-            nextHeights.clear();
-            for (int i = 0; i < heightsN.size(); i++) {
-                Pair current = heightsN.get(i);
-                int upI = current.i-1, upJ = current.j;
-                int downI = current.i+1, downJ = current.j;
-                int leftI = current.i, leftJ = current.j-1;
-                int rightI = current.i ,rightJ = current.j+1;
-
-                if(upI>=0){
-                    if(heights[upI][upJ]==-1){
-                        heights[upI][upJ] = currentHeight+1;
-                        nextHeights.add(new Pair(upI,upJ));
-                    }
+            if(upI>=0){
+                if(heights[upI][upJ]==-1){
+                    heights[upI][upJ] = currentHeight+1;
+                    frontier.add(new int[]{upI,upJ});
                 }
-                if(downI<heights.length){
-                    if(heights[downI][downJ]==-1){
-                        heights[downI][downJ] = currentHeight+1;
-                        nextHeights.add(new Pair(downI,downJ));
-                    }
-                }
-                if(leftJ>=0){
-                    if(heights[leftI][leftJ]==-1){
-                        heights[leftI][leftJ] = currentHeight+1;
-                        nextHeights.add(new Pair(leftI,leftJ));
-                    }
-                }
-                if(rightJ<heights[0].length){
-                    if(heights[rightI][rightJ]==-1){
-                        heights[rightI][rightJ] = currentHeight+1;
-                        nextHeights.add(new Pair(rightI,rightJ));
-                    }
-                }
-
             }
-            currentHeight++;
-            heightsN = new ArrayList<>(nextHeights);
-/*
-
-            System.out.println("--------------------------------------");
-            for (int i = 0; i < heights.length; i++) {
-                System.out.println(Arrays.toString(heights[i]));
+            if(downI<heights.length){
+                if(heights[downI][downJ]==-1){
+                    heights[downI][downJ] = currentHeight+1;
+                    frontier.add(new int[]{downI,downJ});
+                }
             }
-*/
-            if(heightsN.size()==0){
-                break;
+            if(leftJ>=0){
+                if(heights[leftI][leftJ]==-1){
+                    heights[leftI][leftJ] = currentHeight+1;
+                    frontier.add(new int[]{leftI,leftJ});
+                }
+            }
+            if(rightJ<heights[0].length){
+                if(heights[rightI][rightJ]==-1){
+                    heights[rightI][rightJ] = currentHeight+1;
+                    frontier.add(new int[]{rightI,rightJ});
+                }
             }
 
         }
 
         return heights;
-    }
-
-
-
-    public static class Pair{
-
-        public int i;
-        public int j;
-
-        public Pair(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-
-        @Override
-        public String toString() {
-            return "Pair{" +
-                    "i=" + i +
-                    ", j=" + j +
-                    '}';
-        }
     }
 
 }
