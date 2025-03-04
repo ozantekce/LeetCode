c3af3de19a87da64f0376d171e666a10556c84db
ozantekce <ozantekce@gmail.com>
2025-03-04 21:46:44 +0300

    2413. Smallest Even Multiple

---
+++ src/Smallest_Even_Multiple_2413/Solution.java
@@ -0,0 +1,11 @@
+package Smallest_Even_Multiple_2413;
+
+public class Solution {
+
+
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) return n;
+        else return n * 2;
+    }
+
+}
