681697f535e9c295db214c81c47defa7d90507ad
ozantekce <ozantekce@gmail.com>
2025-03-04 22:06:20 +0300

    1480. Running Sum of 1d Array

---
+++ src/Running_Sum_of_1d_Array_1480/Solution.java
@@ -0,0 +1,12 @@
+package Running_Sum_of_1d_Array_1480;
+
+public class Solution {
+
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+
+}
