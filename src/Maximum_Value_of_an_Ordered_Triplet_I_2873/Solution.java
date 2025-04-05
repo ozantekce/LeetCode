0037a5af392fcbbea89a40856f3926b58c81d2be
ozantekce <ozantekce@gmail.com>
2025-04-05 19:28:55 +0300

    Maximum Value of an Ordered Triplet I

---
+++ src/Maximum_Value_of_an_Ordered_Triplet_I_2873/Solution.java
@@ -0,0 +1,30 @@
+package Maximum_Value_of_an_Ordered_Triplet_I_2873;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println(maximumTripletValue(new int[]{12, 6, 1, 2, 7}));
+        System.out.println(maximumTripletValue(new int[]{1, 10, 3, 4, 19}));
+    }
+
+
+    public static long maximumTripletValue(int[] nums) {
+        int[] temp = new int[nums.length];
+
+        int prefixMax = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            prefixMax = Math.max(nums[i - 1], prefixMax);
+            temp[i] = prefixMax - nums[i];
+        }
+
+        long res = 0;
+        int suffixMax = 0;
+        for (int i = nums.length - 2; i >= 1; i--) {
+            suffixMax = Math.max(nums[i + 1], suffixMax);
+            res = Math.max(res, (long) temp[i] * suffixMax);
+        }
+
+        return res;
+    }
+}
